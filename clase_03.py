da0de372f1bd4cba40ead11a26b86fe261d0217b
Cristian Javier <c.javier.lima@gmail.com>
2025-09-15 21:54:57 -0600

    clase 3 en python

---
+++ clase_03.py
@@ -0,0 +1,14 @@
+x = 10
+y = 5.678
+z= 1.2E6 #notacion cientifica
+a = 1.2E-6 #notacion cientifica recorriendo la coma 6 lugares a la izquierda
+print(type(x)) #type es una funcion que muestra el tipo de dato de la variable
+print(type(y))
+print(z)
+print(a)
+
+is_true = True #booleano
+is_false = False
+print(type(is_true))
+
+#en esta clase se vio el uso de tipos de datos numericos y booleanos
