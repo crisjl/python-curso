e5be99e1cf39f376383f0e05025cc7c1de9ced9d
Cristian Javier <c.javier.lima@gmail.com>
2025-09-15 22:58:16 -0600

    clase 6 de python

---
+++ clase_05.py
@@ -0,0 +1,12 @@
+a = 10
+b = 3
+print(a > b) #mayor que
+print(a < b) #menor que
+print(a >= b) #mayor o igual que
+print(a <= b) #menor o igual que
+print(a == b) #igual que
+print(a != b) #diferente que
+#en esta clase se vio el uso de operadores de comparacion y operadores logicos
+print(a > b and a != b) #operador and, devuelve True si ambas condiciones son True
+print(a > b or a == b) #operador or, devuelve True si al menos una condicion es True
+print(not(a > b)) #operador not, invierte el valor de la condicion          
