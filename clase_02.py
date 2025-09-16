bd7c46ce9758deb9b7b892e22ed6cc47e557e564
Cristian Javier <c.javier.lima@gmail.com>
2025-09-15 21:26:17 -0600

    segunda clase en python

---
+++ clase_02.py
@@ -0,0 +1,15 @@
+name = "cristian"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1]) #imprime la ultima letra de la variable
+
+last_name = "javier"
+print(name + " " + last_name) #concatenacion de variables
+print(name*3) #imprime la variable 3 veces "replicacion"
+
+print(len(name)) #len es una funcion que cuenta la cantidad de caracteres de una variable
+print(name.upper()) #convierte la variable a mayusculas
+print(name.lower()) #convierte la variable a minusculas
+
+#en esta clase se vio el uso de indices, slicing, funciones y metodos de strings
