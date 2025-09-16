723e1a815df2c2da52fa4a0743618aa27c5b465b
Cristian Javier <c.javier.lima@gmail.com>
2025-09-15 20:13:08 -0600

    primera clase sobre python

---
+++ clase_01.py
@@ -0,0 +1,8 @@
+#print("Hola mundo")
+#print("hola")
+#print("mundo")
+saludo = "Hola"
+nombre = "cristian" 
+print(saludo + " " + nombre)
+
+#durante la clase se vio el usi de print, comentarios y variables
