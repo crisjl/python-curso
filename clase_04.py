766239865ad57c6a9509133db5a0b10ce7d96dc6
Cristian Javier <c.javier.lima@gmail.com>
2025-09-15 22:34:31 -0600

    clase numero 4 en python

---
+++ clase_04.py
@@ -0,0 +1,23 @@
+# Operaciones aritméticas
+# Suma
+a = 10 
+b = 3 
+print("suma:", a + b)
+# Resta
+print("resta:", a - b)
+# Multiplicación
+print("multiplicacion:", a * b)
+# División
+print("division:", a / b) #devuelve un float    
+# División entera
+print("division entera:", a // b) #devuelve un int
+# Módulo
+print("modulo:", a % b) #devuelve el resto de la division
+# Potencia
+print("potencia:", a ** b) #devuelve a elevado a la b
+# Precedencia de operadores: (), **, *, /, //, %, +, -
+# Se pueden usar paréntesis para cambiar la precedencia
+resultado = (a + b) * (a - b)
+print("resultado:", resultado)
+
+# en esta clase se vio el uso de operadores aritmeticos y la precedencia de operadores
